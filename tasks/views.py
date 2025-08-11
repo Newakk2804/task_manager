8c118cd5b3c8e7dd4ed2d652fc9c5ea94b02ffd1
Newakk2804 <dan28.04@mail.ru>
2025-08-12 00:10:13 +0300

    feat(tasks): add views for CRUD operations to tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
 
-# Create your views here.
+from tasks.models import Task
+from tasks.forms import TaskForm
+from tasks.mixins import OwnerRequiredMixin
+
+
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = "tasks/task_list.html"
+    context_object_name = "tasks"
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Task.objects.filter(owner=self.request.user)
+
+
+class TaskDetailView(LoginRequiredMixin, OwnerRequiredMixin, DetailView):
+    model = Task
+    template_name = "tasks/task_detail.html"
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "tasks/task_form.html"
+    success_url = reverse_lazy("task_list")
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        response = super().form_valid(form)
+        messages.success(self.request, "Задача успешно создана.")
+        return response
+
+
+class TaskUpdateView(LoginRequiredMixin, OwnerRequiredMixin, UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "tasks/task_form.html"
+    success_url = reverse_lazy("task_list")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Задача обновлена.")
+        return super().form_valid(form)
+
+
+class TaskDeleteView(LoginRequiredMixin, OwnerRequiredMixin, DeleteView):
+    model = Task
+    template_name = "tasks/task_confirm_delete.html"
+    success_url = reverse_lazy("task_list")
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, "Задача удалена.")
+        return super().delete(request, *args, **kwargs)
