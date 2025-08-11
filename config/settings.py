d13059182fc24cb1375330334d4f97527a5649f5
Newakk2804 <dan28.04@mail.ru>
2025-08-11 22:22:01 +0300

    feat(custom_user): create custom users models and config redirects urls for users

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.CustomUser"
+
+LOGIN_REDIRECT_URL = "task_list"
+LOGOUT_REDIRECT_URL = "login"
