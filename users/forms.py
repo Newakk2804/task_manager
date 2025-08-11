b9ad8d1b1caef87f79aa7fd3a46d18f4b3aada25
Newakk2804 <dan28.04@mail.ru>
2025-08-12 00:28:34 +0300

    feat(custom_user): change forms for users

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,34 @@
-from django import forms
+# users/forms.py
 from django.contrib.auth.forms import UserCreationForm
-from users.models import CustomUser
+from crispy_forms.helper import FormHelper
+from .models import CustomUser
+
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ("username", "email")
+        fields = ("username", "email", "password1", "password2")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        placeholders = {
+            "username": "Имя пользователя",
+            "email": "Email",
+            "password1": "Пароль",
+            "password2": "Повторите пароль",
+        }
+
+        for name, field in self.fields.items():
+            field.help_text = ""
+            field.label = ""
+            field.widget.attrs.update(
+                {
+                    "placeholder": placeholders.get(name, name.capitalize()),
+                    "class": "form-control",
+                }
+            )
+
+        self.helper = FormHelper()
+        self.helper.form_show_labels = False
+        self.helper.form_method = "post"
