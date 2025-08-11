21c17f1418a7d31e9ec15d5b1bd7921ef2f5e124
Newakk2804 <dan28.04@mail.ru>
2025-08-12 00:11:37 +0300

    feat(tasks): created custom mixin for task's check owner

---
+++ tasks/mixins.py
@@ -0,0 +1,9 @@
+from django.core.exceptions import PermissionDenied
+
+
+class OwnerRequiredMixin:
+    def dispatch(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.owner != request.user:
+            raise PermissionDenied
+        return super().dispatch(request, *args, **kwargs)
