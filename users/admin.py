a481d28e3216994a5c213cfaddf828e93539de8f
Newakk2804 <dan28.04@mail.ru>
2025-08-11 22:23:02 +0300

    feat(custom_user): implemented display to users in admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import CustomUser
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "is_staff", "is_active")
