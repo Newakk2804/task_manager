bf9073e8bf78bafcdde9d1e67813213cb083956f
Newakk2804 <dan28.04@mail.ru>
2025-08-12 00:14:02 +0300

    feat(tasks): add urls for tasks

--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("users.urls")),
-    path('admin/', admin.site.urls),
+    path("", include("tasks.urls")),
 ]
