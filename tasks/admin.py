ca561b571067cd905262292b5aa2622f5a5e503d
Newakk2804 <dan28.04@mail.ru>
2025-08-12 00:05:45 +0300

    feat(tasks): implemented display to tasks in admin panel

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from tasks.models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "owner", "completed", "priority", "due_date", "created_at")
+    list_filter = ("completed", "priority")
+    search_fields = ("title", "description", "owner__username")
